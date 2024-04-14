c9b00692137028b6db9e1fe69663d53ad4ba276e
raffaeledanna <rafdanna28@gmail.com>
2024-04-14 15:45:00 +0200

    start project

---
+++ src/test/java/GildedRoseTest.java
@@ -0,0 +1,21 @@
+import org.gildedrose.GildedRose;
+import org.gildedrose.Item;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GildedRoseTest {
+    @Test
+    void testUpdateQualityOnAgedBrie() {
+        Item[] item = { new Item("Aged Brie", 10, 20)};
+        GildedRose gr = new GildedRose(item);
+
+        gr.updateQuality();
+
+        assertEquals(gr.getItems()[0].sellIn, 9);
+        assertEquals(gr.getItems()[0].quality, 21);
+
+
+    }
+
+}
